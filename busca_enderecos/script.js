b6b1be48bd8b50b5acafc9b4684bed103bac8f4d
Lucas Cividini <lucaspagnussat5@gmail.com>
2024-03-07 20:30:39 -0300

    final adjusts

--- busca_enderecos/script.js
+++ busca_enderecos/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    alert(123)
   $('body').on('click', '#consultar', function(){
       consultaCep();
   });
@@ -24,19 +23,19 @@ $(document).ready(function(){
               console.log(data);
               $('body').find('#resultados').html('<h2>resultado da consulta</h2>' 
                                                                     + '<p> CEP:'+ data.cep+ '</p1>'
-                                                                    + '<p> Logradouro' + data.logradouro + '</p>'
-                                                                    + '<p> Bairro' + data.bairro+ '</p>' 
-                                                                    + '<p> Cidade' + data.localidade+ '</p>' 
-                                                                    + '<p> Estado' + data.uf+ '</p>' 
-                                                                    + '<p> IBGE' + data.ibge+ '</p>');
+                                                                    + '<p> Logradouro:' + data.logradouro + '</p>'
+                                                                    + '<p> Bairro:' + data.bairro+ '</p>' 
+                                                                    + '<p> Cidade:' + data.localidade+ '</p>' 
+                                                                    + '<p> Estado:' + data.uf+ '</p>' 
+                                                                    + '<p> IBGE:' + data.ibge+ '</p>');
 
           }
       })
-    }, 2000)
+    }, 1000)
   }
 
   function loading(){
-    $('resultado').html('<img src="loading.gif"/> ')
+    $('#resultados').html('<img src="loading.gif"/> ')
   }
 
 
